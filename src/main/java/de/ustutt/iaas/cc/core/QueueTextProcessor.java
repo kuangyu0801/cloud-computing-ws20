692db04b28bc964025c214cc6265b300f06d7283
Florian Haupt <F7502@users.noreply.github.com>
2017-05-12 17:48:51 +0200

    implement queue-based load-balancing for text processors

--- src/main/java/de/ustutt/iaas/cc/core/QueueTextProcessor.java
+++ src/main/java/de/ustutt/iaas/cc/core/QueueTextProcessor.java
@@ -1,8 +1,15 @@
 package de.ustutt.iaas.cc.core;
 
 import java.util.Hashtable;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ExecutionException;
 
 import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
 import javax.jms.Queue;
 import javax.jms.QueueConnection;
 import javax.jms.QueueConnectionFactory;
@@ -15,14 +22,20 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.amazon.sqs.javamessaging.SQSConnectionFactory;
 import com.amazonaws.auth.PropertiesFileCredentialsProvider;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
+import com.google.common.base.Strings;
 
 import de.ustutt.iaas.cc.TextProcessorConfiguration;
 
 public class QueueTextProcessor implements ITextProcessor {
+
+    private final static Logger logger = LoggerFactory.getLogger(QueueTextProcessor.class);
 
     private QueueSession session;
     private QueueSender sender;
@@ -82,6 +95,7 @@ public class QueueTextProcessor implements ITextProcessor {
 	    // create sender and receiver
 	    sender = session.createSender(requestQueue);
 	    receiver = session.createReceiver(responseQueue);
+	    receiver.setMessageListener(new ML());
 	    // start connection (!)
 	    connection.start();
 	} catch (NamingException e) {
@@ -93,24 +107,69 @@ public class QueueTextProcessor implements ITextProcessor {
 	}
     }
 
+    private static ConcurrentMap<String, CompletableFuture<String>> work = new ConcurrentHashMap<String, CompletableFuture<String>>();
+    private static final String ID_PROP = "id";
+
     @Override
     public String process(String text) {
-	// TODO send message to request queue, receive response from response
-
-	// Create a text message like this:
-	// TextMessage msg = session.createTextMessage();
-
-	// Add properties to a message like this:
-	// msg.setStringProperty("...", "...");
-	// Note: The text processor will copy all properties from the request
-	// message to the response message
-
-	// Receive messages either like this:
-	// receiver.receive();
-	// Or add a message listener instead:
-	// receiver.setMessageListener(new MyMessageLister());
-	
+	// generate unique ID for request
+	String id = UUID.randomUUID().toString();
+	// store future in work map
+	CompletableFuture<String> cf = new CompletableFuture<String>();
+	work.put(id, cf);
+	try {
+	    Message msg = session.createTextMessage(text);
+	    msg.setStringProperty(ID_PROP, id);
+	    logger.debug("Sending message {}", msg.getStringProperty(ID_PROP));
+	    sender.send(msg);
+	} catch (JMSException e) {
+	    // TODO maybe remove future from work?
+	    e.printStackTrace();
+	}
+	// wait for result
+	try {
+	    // blocking wait until future is completed
+	    String result = cf.get();
+	    return result;
+	} catch (InterruptedException e) {
+	    e.printStackTrace();
+	} catch (ExecutionException e) {
+	    e.printStackTrace();
+	}
+	// in case of errors, return original (unprocessed) text
 	return text;
+    }
+
+    private static class ML implements MessageListener {
+
+	@Override
+	public void onMessage(Message message) {
+	    if (message instanceof TextMessage) {
+		try {
+		    // get id for correlation
+		    String id = message.getStringProperty(ID_PROP);
+		    if (Strings.isNullOrEmpty(id)) {
+			logger.warn("Received message without ID for correlation.");
+			return;
+		    }
+		    logger.debug("Received message {}", id);
+		    
+		    // get corresponding future
+		    CompletableFuture<String> cf = work.get(id);
+		    if (cf == null) {
+			logger.warn("Cannot find request for response {}", id);
+			return;
+		    }
+		    // complete future
+		    cf.complete(((TextMessage) message).getText());
+		    // remove from map
+		    work.remove(id);
+		} catch (JMSException e) {
+		    e.printStackTrace();
+		}
+	    }
+	}
+
     }
 
 }
